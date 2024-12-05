175011b6dbdda169bd1308a1c3825d09ed3631c1
Martin Kinkelin <mkinkelin@symmetryinvestments.com>
2024-12-06 00:27:38 +0100

    druntime: Enable DRT_* environment variables support by default
    In order to trivially switch between GCs.

--- runtime/druntime/src/rt/config.d
+++ runtime/druntime/src/rt/config.d
@@ -49,7 +49,7 @@ version (LDC) // use @weak
 
     extern(C) __gshared @weak
     {
-        pragma(mangle, "rt_envvars_enabled") bool _rt_envvars_enabled = false;
+        pragma(mangle, "rt_envvars_enabled") bool _rt_envvars_enabled = true; // Symmetry-specific
         pragma(mangle, "rt_cmdline_enabled") bool _rt_cmdline_enabled = true;
         pragma(mangle, "rt_options") string[] _rt_options = [];
     }
